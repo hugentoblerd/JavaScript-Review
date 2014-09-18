ab489ed80b65648022f8992526115d43cfb09212
David Hugentobler <davidhugentobler30@gmail.com>
2014-09-17 20:20:14 -0600

    completes callbacksReview-two.js

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ JavaScript-Review
 ##### Fork this repo, clone your fork, then do all of the tasks in the following order.
 1. arrayReview.js --> DONE
 2. objectReview.js --> DONE
-3. callbackReview.js --> 
+3. callbackReview.js --> DONE
 4. closureReview.js --> 
 5. constructorReview.js -->
--- callbacksReview-two.js
+++ callbacksReview-two.js
@@ -37,12 +37,27 @@ var songs =
 //Above you have a large Array of Song Objects. 
 //Write a 'getter' function that returns the songs array.
 
-  //code here
+_____MyCode______________
 
+var getter = function (arr) {
+    return arr;
+}
+
+___________________________
 
 //Now write a setter that takes in two paramaters.
 //The first parameter is a song object and the second parameter is a callback (which will be a placeholder for your getter function)
 //Inside your setter function first add the first parameter, or the song object, to the end of your songs array.
 //Once you do that, call your callback (getter) function which should get the songs and return them.
   
-  //code here
+_____MyCode______________
+
+var setter = function (obj, cb) {
+    songs.push(obj);
+    cb(songs);
+}
+
+___________________________
+
+
+
