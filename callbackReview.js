57217d7074daa47aab5d1957d5d8aa7bb54a0d4b
David Hugentobler <davidhugentobler30@gmail.com>
2014-09-17 20:06:34 -0600

    completes callbackReview.js

--- callbackReview.js
+++ callbackReview.js
@@ -1,6 +1,13 @@
 /* Declare and Define the functions here that will make the function calls below work properly */
 
 
+// _______MyCode________
+
+var first = function (arr, cb) {
+  cb(arr[0]);
+};
+
+// _______________________
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 first(names, function(firstName){
@@ -8,12 +15,16 @@ first(names, function(firstName){
 });
 
 
-
-
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
+// _______MyCode________
 
+var last = function (arr, cb) {
+  cb(arr[arr.length - 1]);
+}
+
+// _______________________
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 last(names, function(lastName){
@@ -26,14 +37,26 @@ last(names, function(lastName){
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
+// _______MyCode________
 
+var contains = function (str, cb) {
+  var inNames = false;
+  for (var i = 0; i < str.length; i++) {
+    if (str === names[i]) {
+      inNames = true;
+    };
+  };
+  cb(inNames);
+}
+
+// _______________________
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 contains('Colt', function(yes){
   if(yes){
-    'Colt is in the array';
+    console.log('Colt is in the array');
   } else {
-    'Colt is not in the list';
+    console.log('Colt is not in the list');
   }
 });
 
@@ -43,11 +66,21 @@ contains('Colt', function(yes){
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
+// _______MyCode________
 
+var map = function (arr, cb) {
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    newArr[i] = cb(arr[i]);
+  };
+  return newArr;
+}
+
+// _______________________
 
 var numbers = [1,2,3,4,5];
 //Produces a new array of values by mapping each value in list through a transformation function
-map(, function(num){
+map(numbers, function(num){
   return num * 2; //returns an array of [2,4,6,8,10]
 });
 
@@ -57,7 +90,31 @@ map(, function(num){
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
+// _______MyCode________
 
+var uniq = function (arr, cb) {
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    newArr.push(arr[i]);
+  };
+  for (var i = 0; i < newArr.length; i++) {
+    for (var j = 0; j < newArr.length; j++) {
+      if (i !== j) {
+        if (newArr[i] === newArr[j]) {
+          newArr.splice(j, 1);
+          j--;
+        };
+      };
+    };
+  };
+  cb(newArr);
+}
+
+// OR
+
+
+
+// _______________________
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 uniq(names, function(uniqArr){
@@ -70,11 +127,19 @@ uniq(names, function(uniqArr){
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
+// _______MyCode________
 
+var each = function (arr, cb) {
+  for (var i = 0; i < arr.length; i++) {
+    cb(arr[i], i)
+  };
+}
+
+// _______________________
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 each(names, function(item, indice){
-  console.log('The item in the ' + indice + 'position is ' + item)
+  console.log('The item in the ' + indice + ' position is ' + item)
 });
 
 
@@ -83,7 +148,20 @@ each(names, function(item, indice){
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
+// _______MyCode________
 
+var getUserById = function (str, cb) {
+  for (var i = 0; i < users.length; i++) {
+    for (key in users[i]) {
+      debugger;
+      if (users[i][key] === str) {
+        cb(users[i]);
+      };
+    };
+  };
+}
+
+// _______________________
 
 var users = [
   {
@@ -106,7 +184,7 @@ var users = [
   },
 ];
 getUserById('16t', function(user){
-  console.log('The user with the id 16t has the email of ' + user.email + 'the name of ' + user.name + ' and the address of ' + user.address); 
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
 });
 
 
@@ -115,7 +193,19 @@ getUserById('16t', function(user){
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
+// _______MyCode________
 
+var find = function (arr, cb) {
+  for (var i = 0; i < arr.length; i++) {
+    if (cb(arr[i])) {
+      return arr[i];
+    }else {
+      return false;
+    };
+  };
+}
+
+// _______________________
 
 //Looks through each value in the list, returning the first one that passes a truth test 
 var numbers  = [1, 2, 3, 4, 5, 6];
